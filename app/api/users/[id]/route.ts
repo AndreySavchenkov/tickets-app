196078f57874062383d28eacb02641774cad8e52
AndreySavchenkov <sannndvik16@gmail.com>
2024-05-22 18:53:51 +0200

    feat: update user

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -7,7 +7,7 @@ interface Props {
   params: { id: string };
 }
 
-export async function PATH(request: NextRequest, { params }: Props) {
+export async function PATCH(request: NextRequest, { params }: Props) {
   const body = await request.json();
   const validation = userSchema.safeParse(body);
 
@@ -23,14 +23,16 @@ export async function PATH(request: NextRequest, { params }: Props) {
     return NextResponse.json({ error: "User Not Found" }, { status: 404 });
   }
 
-  if (body?.password) {
+  if (body?.password && body.password != "") {
     const hashPassword = await bcrypt.hash(body.password, 10);
     body.password = hashPassword;
+  } else {
+    delete body.password;
   }
 
   if (user.username !== body.username) {
     const userDuplicate = await prisma.user.findUnique({
-      where: { id: body.username },
+      where: { username: body.username },
     });
 
     if (userDuplicate) {
