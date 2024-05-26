3aa535514275e6b09bdb859bed1aa2f7dae8c056
AndreySavchenkov <sannndvik16@gmail.com>
2024-05-26 06:56:39 +0200

    feat: dashboard setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,46 @@
+import DashChart from "@/components/DashChart";
+import DashRecentTickets from "@/components/DashRecentTickets";
+import prisma from "@/prisma/db";
 import React from "react";
 
-const Dashboard = () => {
-  return <div>Dashboard</div>;
+const Dashboard = async () => {
+  const tickets = await prisma.ticket.findMany({
+    where: {
+      NOT: [{ status: "CLOSED" }],
+    },
+    orderBy: {
+      updatedAt: "desc",
+    },
+    skip: 0,
+    take: 5,
+    include: {
+      assignedToUser: true,
+    },
+  });
+
+  const groupTickets = await prisma.ticket.groupBy({
+    by: ["status"],
+    _count: {
+      id: true,
+    },
+  });
+
+  console.log(groupTickets);
+
+  console.log(tickets);
+
+  return (
+    <div>
+      <div className="grid gap-4 md:grid-cols-2 px-2">
+        <div>
+          <DashRecentTickets />
+        </div>
+        <div>
+          <DashChart />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
